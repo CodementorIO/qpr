305fdc45534e36b2a6feaebf41e2676255f12231
Yang-Hsing Lin <yanghsing.lin@gmail.com>
2017-04-04 21:51:49 +0800

    reference credentials from home dir

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import queries from './queries'
 import runner from './runner'
 import yaml from 'js-yaml'
 import fs from 'fs'
+import os from 'os'
 import path from 'path'
 
 const { username, token, repoOwner } = yaml.safeLoad(
-  fs.readFileSync(path.join(__dirname, '..', '.credentials.yml'), 'utf8'))
+  fs.readFileSync(path.join(os.homedir(), '.qpr.credentials.yml'), 'utf8'))
 
 runner({ username, token, queries: queries({ username, repoOwner }) })
