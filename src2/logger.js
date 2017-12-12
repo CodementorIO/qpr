4820e34523aa92cf4878b4dba46a3e4e2a204e4c
Yang-Hsing Lin <yanghsing.lin@gmail.com>
2017-12-12 23:38:09 +0800

    fix logger

--- src2/logger.js
+++ src2/logger.js
@@ -1,11 +1,14 @@
 import _colors from 'colors'
+import getObj from 'lodash.get'
 
 export default ({ queryName, items })=> {
   if (items.length === 0) {
     return
   }
   console.log(`=== ${queryName} ===`.bold)
-  items.forEach(({ title, url })=> {
+  items.forEach(item => {
+    let title = item.title
+    let url = getObj(item, ['pull_request', 'html_url'])
     console.log(` ${'*'.red} ${title}`)
     console.log(`   ${url}`)
   })
